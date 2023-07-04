761f3368698840c383ce5293c7d377bc3a89a94a
fattony01 <a.kondratenko.dev@gmail.com>
2023-07-04 23:41:01 +0200

    the first task is done without random

--- HW_ContactList/Model/ Person.swift
+++ HW_ContactList/Model/ Person.swift
@@ -6,5 +6,27 @@
 //
 
 struct Person {
+    let person = DataStore()
     
+    var firstName: String
+    var lastName: String
+    var phone: String
+    var email: String
+    
+    
+    static func getPersons(names: [String],
+                           surnames: [String],
+                           phones: [String],
+                           mails: [String]) -> [Person] {
+        var persons: [Person] = []
+        
+        for index in 0..<min(names.count, surnames.count, phones.count, mails.count) {
+            let person = Person(firstName: names[index],
+                                lastName: surnames[index],
+                                phone: phones[index],
+                                email: mails[index])
+            persons.append(person)
+        }
+        return persons
+    }
 }
--- HW_ContactList/ViewControllers/ContactInfoViewController.swift
+++ HW_ContactList/ViewControllers/ContactInfoViewController.swift
@@ -8,22 +8,15 @@
 import UIKit
 
 class ContactInfoViewController: UIViewController {
+    @IBOutlet var phoneLabel: UILabel!
+    @IBOutlet var mailLabel: UILabel!
+    
+    var person: Person!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        phoneLabel.text = person.phone
+        mailLabel.text = person.email
+        title = "\(person.firstName) \(person.lastName)"
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
