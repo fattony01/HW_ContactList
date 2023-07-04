e39a95e7bac9372b29d66b6c159b8dc01a958eac
fattony01 <a.kondratenko.dev@gmail.com>
2023-07-04 21:46:33 +0200

    start

---
+++ HW_ContactList/Model/ Person.swift
@@ -0,0 +1,10 @@
+//
+//   Person.swift
+//  HW_ContactList
+//
+//  Created by Anton Kondratenko on 04.07.2023.
+//
+
+struct Person {
+    
+}
---
+++ HW_ContactList/Services/DataStore.swift
@@ -0,0 +1,12 @@
+//
+//  DataStore.swift
+//  HW_ContactList
+//
+//  Created by Anton Kondratenko on 04.07.2023.
+//
+
+import Foundation
+
+class DataStore {
+    
+}
